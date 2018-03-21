46f43bf713af0835ec3a42e695edf57bf64106f6
huangjian <huangjian1820@gmail.com>
2018-03-22 01:44:36 +0800

    feat(project): update babel to presets-env & fixed webpack warnings
    Change-Id: I98fe4eb8b9f8cbe21e36329e950994f5e3358946

--- webpack.config.js
+++ webpack.config.js
@@ -3,89 +3,84 @@ var webpack = require("webpack");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: [
-        "react-hot-loader/patch",
-        "webpack-dev-server/client?http://0.0.0.0:3000",
-        "webpack/hot/only-dev-server",
-        "babel-polyfill",
-        "whatwg-fetch",
-        "./src/index"
-    ],
-    devServer: {
-        hot: true,
-        contentBase: path.resolve(__dirname, "dist"),
-        port: 3000,
-        host: "0.0.0.0",
-        publicPath: "/",
-        historyApiFallback: true,
-        disableHostCheck: true
-    },
-    output: {
-        path: path.join(__dirname, "dist"),
-        publicPath: "/",
-        filename: "app.[hash].js"
-    },
-    devtool: "eval",
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: "babel-loader",
-                options: {
-                    presets: [
-                        ["es2015", {"modules": false}],
-                        "stage-0",
-                        "react"
-                    ],
-                    plugins: [
-                        "transform-async-to-generator",
-                        "transform-decorators-legacy"
-                    ]
-                }
-            },
-            {
-                test: /\.scss|css$/,
-                use: [
-                    "style-loader",
-                    "css-loader",
-                    "postcss-loader",
-                    "resolve-url-loader",
-                    "sass-loader?sourceMap"
-                ]
-            },
-            {
-                test: /\.(jpe?g|png|gif|svg)$/i,
-                use: [
-                    "file-loader?hash=sha512&digest=hex&name=[hash].[ext]",
-                    {
-                        loader: "image-webpack-loader",
-                        options: {
-                            progressive: true,
-                            optimizationLevel: 7,
-                            interlaced: false,
-                            pngquant: {
-                                quality: "65-90",
-                                speed: 4
-                            }
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                use: "url-loader?limit=10000&mimetype=application/font-woff"
-            },
-            {
-                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                use: "file-loader"
-            }
+  entry: [
+    "react-hot-loader/patch",
+    "webpack-dev-server/client?http://0.0.0.0:3000",
+    "webpack/hot/only-dev-server",
+    "whatwg-fetch",
+    "./src/index"
+  ],
+  devServer: {
+    hot: true,
+    contentBase: path.resolve(__dirname, "dist"),
+    port: 3000,
+    host: "0.0.0.0",
+    publicPath: "/",
+    historyApiFallback: true,
+    disableHostCheck: true
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    publicPath: "/",
+    filename: "app.[hash].js"
+  },
+  devtool: "cheap-eval-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.scss|css$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "postcss-loader?sourceMap",
+          "resolve-url-loader",
+          "sass-loader?sourceMap"
         ]
-    },
-    plugins: [
-        new webpack.NamedModulesPlugin(),
-        new webpack.HotModuleReplacementPlugin(),
-        new HtmlWebpackPlugin({ hash: false, template: "./index.hbs" }),
-        new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /nb/)
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          "file-loader?hash=sha512&digest=hex&name=[hash].[ext]",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              optipng: {
+                optimizationLevel: 7
+              },
+              gifsicle: {
+                interlaced: false
+              },
+              pngquant: {
+                quality: '65-90',
+                speed: 4,
+              },
+              mozjpeg: {
+                quality: 65,
+                progressive: true
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: "url-loader?limit=10000&mimetype=application/font-woff"
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: "file-loader"
+      }
     ]
+  },
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({hash: false, template: "./index.hbs"}),
+    new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /nb/)
+  ]
 };
